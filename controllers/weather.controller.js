273830139b2390a7bc40e2c148570b0d2a516178
ibrahemomari <ibrahem.omari96@gmail.com>
2021-07-01 17:40:28 +0300

    cashing

--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -1,27 +1,55 @@
-
 require('dotenv').config();
 const WEATHER_BIT_KEY=process.env.WEATHER_BIT_KEY;
 const axios = require('axios');
 const Forecast=require('../Modules/weather.model')
+const Cache=require('../tools/cache');
 
+let cache=new Cache();
+cache['data']=[];
+cache['timesstamp']=Date.now();
+cache['lastUpdate']='';
+let curentDay=()=>{
+    let today = new Date();
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        let dateTime = date+' '+time;
+        return dateTime;
+}
 const weatherController=(req,res)=>{
-    try{
     let weather;
     let lat=req.query.lat;
     let lon=req.query.lon;
-    if(lat.length!==0 && lon.length!==0){
-    let weatherBitLinke=`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
-    let weatherBitRes=axios.get(weatherBitLinke).then(response=>{
-        weather=response.data;
-        let forecast=weather.data.map((item,idx)=>{
-            return new Forecast(item);
-        })
-        res.json(forecast);
-    })
-}else{
-    res.json({error:'error'})
-}} catch{
-    res.json({error:'error'})
+    let forecast=[];
+    setTimeout(()=>{
+        cache['data']=[];
+        cache.lastUpdate=curentDay();
+    },86400000);
+    try{
+    if(lat && lon && lat.length!==0 && lon.length!==0){
+        if(cache.data.length>0){
+            forecast=cache.data.map(el=> new Forecast(el));
+            forecast.push({lastUpdate:cache.lastUpdate});
+            console.log('======== data from cache =========');
+            res.json(forecast);
+        }else{
+            if(lat.length!==0 && lon.length!==0 ){
+                let weatherBitLinke=`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
+                let weatherBitRes=axios.get(weatherBitLinke).then(response=>{
+                    weather=response.data;
+                        forecast=response.data.data.map((item,idx)=>{
+                        return new Forecast(item);
+                    })
+                    forecast.push({lastUpdate:curentDay()});
+                    cache['data']=weather.data;
+                    console.log('======== data from API =========');
+                    res.json(forecast);
+                })
+            }else{
+                res.json({error:'error1'})
+            }
+        }
+    }} catch{
+    res.json({error:'error2'})
 }
 }
 
